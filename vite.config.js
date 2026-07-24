dc36c75cd087f1d7668e5c7952b22ad7819a3322
hyro <batistajosue957@gmail.com>
2026-07-23 23:59:46 -0300

    trocando o base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 export default defineConfig({
+    base: '/pastelecia/',
     plugins: [react()],
-    base: '/pastel-e-cia/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-        base: '/projetos-web',
     },
 });
